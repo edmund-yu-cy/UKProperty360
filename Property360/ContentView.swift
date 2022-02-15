9144d808121e62a93f870e8de30b974fbe48f298
Edmund Yu <edmund.yu@appassionately.com>
2022-02-15 15:54:08 +0800

    Initial Commit

---
+++ Property360/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Property360
+//
+//  Created by Edmund Yu on 15/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Property360/Property360App.swift
@@ -0,0 +1,17 @@
+//
+//  Property360App.swift
+//  Property360
+//
+//  Created by Edmund Yu on 15/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct Property360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
